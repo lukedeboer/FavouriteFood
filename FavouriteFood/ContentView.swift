f8ed29b6ec2b5644d6bc7c364480bd764b4372d4
lukedeboer <lukedeboer48@gmail.com>
2021-03-29 19:14:58 +1000

    Initial Commit

---
+++ FavouriteFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteFood
+//
+//  Created by Luke De boer on 29/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavouriteFood/FavouriteFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteFoodApp.swift
+//  FavouriteFood
+//
+//  Created by Luke De boer on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
